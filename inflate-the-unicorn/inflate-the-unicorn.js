1d40f1ce0790edb058dc4b30d4f8d1736ab8aab6
johannbesas <johannbesas@gmail.com>
2021-03-07 20:35:22 +1300

    Completed inflate the unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,25 @@
   // -    -   -   -   -  //
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+function inflateUnicorn(clicked_id){  
+  if(clicked_id){
+  currentUnicorn = document.getElementById(clicked_id);
+  console.log(currentUnicorn.src);
 
-console.log("Inflate The Unicorn!")
+  var digit = currentUnicorn.src.slice(-5);
+  
+  if (digit === "0.png"){
+    currentUnicorn.src = "./images/unicorn-1.png"
+  }
+  else if (digit === "1.png"){
+    currentUnicorn.src = "./images/unicorn-2.png"
+  }
+  else if (digit === "2.png"){
+    currentUnicorn.src = "./images/unicorn-3.png"
+  }
+  else {
+    var unicornNum = currentUnicorn.id.slice(-1);
+    alert("Unicorn " + unicornNum + " thanks you!");
+  }
+  }
+}
